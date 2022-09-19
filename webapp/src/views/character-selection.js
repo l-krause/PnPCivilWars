60947eccef1f4defb04edfa7a24498dee926bd76
sda <sda>
2022-09-19 22:38:40 +0200

    added textfield

--- webapp/src/views/character-selection.js
+++ webapp/src/views/character-selection.js
@@ -69,6 +69,9 @@ export default function CharacterSelection(props) {
 
     const onChooseCharacter = useCallback(() => {
         setError(null);
+        if (selectedCharacter === "Crab") {
+            setDmPw(true);
+        }
         api.onChooseCharacter(selectedCharacter, (response) => {
             if (response.success) {
                 onSelectCharacter(response.data);
